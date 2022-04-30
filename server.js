30dbd9a445da981309b76b8b2ab1b2a68848bf71
boedegoat <bhremada.fka@gmail.com>
2022-04-30 08:10:25 +0700

    feat: create POST /api/fileanalyse

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const multer = require('multer')
 
-var app = express();
+const app = express()
+const upload = multer()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+// upload.single('<file field name from req.body>')
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname: name, mimetype: type, size } = req.file
+  res.json({ name, type, size })
+})
 
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
